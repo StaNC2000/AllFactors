c4de4e5a0954ddebdafbc4a9883f5d6a2ae2f0b4
es <StaNC2000>
2020-01-25 11:37:53 +0100

    Method printFactors created and tested

--- src/com/eS/AllFactors.java
+++ src/com/eS/AllFactors.java
@@ -2,7 +2,29 @@ package com.eS;
 
 public class AllFactors {
 
+    public static void printFactors(int number){
+        if (number < 1){
+            System.out.println("Invalid Value");
+        }
+
+        for (int divider = 1; divider <= number; divider++){
+            if(number % divider == 0){
+                System.out.println(divider);
+            }
+            else {
+//                System.out.println("Not divide " + divider);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // write your code here
+
+        printFactors(6);
+        printFactors(-6);
+        printFactors(-1);
+        printFactors(16);
+        printFactors(10);
+        printFactors(143);
     }
 }
